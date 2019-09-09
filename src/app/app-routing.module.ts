65ad091b72aa9111bc7e4b0675a0941ceb4e6572
MrShree <38486236+MrShree@users.noreply.github.com>
2019-09-09 20:59:20 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TestComponent } from './test/test.component';
+import { BankComponent } from './bank/bank.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: BankComponent},
+  {path: 'test', component: TestComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BankComponent } from './bank/bank.component';
+import { TestComponent } from './test/test.component';
+import { PipePipe } from './pipe.pipe';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BankComponent,
+    TestComponent,
+    PipePipe,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
